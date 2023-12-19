26399220565fc820a69016944df02cf47b74290b
KKudas <22100890@usc.edu.ph>
2023-12-19 11:01:26 +0800

    Mute button implemented

--- scripts/game.gd
+++ scripts/game.gd
@@ -6,7 +6,8 @@ class_name Game
 @onready var spawn = $SpawnPoint
 @onready var checkpoint = $Map/Checkpoint
 @onready var transition = $transition
-
+@onready var textmute = $Mute/Panel/Button/TextureRect
+var mute = false
 
 func _ready():
 	Global.spawnpoint = player.global_position
@@ -15,3 +16,14 @@ func _process(_delta):
 	if checkpoint.activated:
 		get_tree().change_scene_to_packed(Global.victory)
 
+
+
+func _on_button_pressed():
+	if mute:
+		Globaltheme.stream_paused = false
+		textmute.texture = load("res://assets/menu/mute.svg")
+	else:
+		Globaltheme.stream_paused = true
+		textmute.texture = load("res://assets/menu/unmute.svg")
+	
+	mute = !mute
